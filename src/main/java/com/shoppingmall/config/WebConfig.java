948610e15780302921b7ce265ab0e925aa3b8efe
SHIN SUJUNG <shinssjj79@gmail.com>
2024-02-27 19:03:00 +0900

    feat : WebConfig 클래스 추가를 통한 정적 리소스 경로 설정

---
+++ src/main/java/com/shoppingmall/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.shoppingmall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/views/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/views/js/");
+    }
+}
--- src/main/java/com/shoppingmall/controller/MainController.java
+++ src/main/java/com/shoppingmall/controller/MainController.java
@@ -2,6 +2,7 @@ package com.shoppingmall.controller;
 
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +14,7 @@ import com.shoppingmall.dto.GoodsDTO;
 import com.shoppingmall.service.GoodsService;
 
 @Controller
+@Slf4j
 public class MainController {
 
 	@Autowired
@@ -20,9 +22,7 @@ public class MainController {
 
 	@GetMapping("/") // 리스트 무조건 뿌리기
 	public String mainView(Model model) {
-		System.out.println("model = " + model);
 		List<GoodsDTO> list = service.goodsList("top");
-		System.out.println("list = " + list);
 		model.addAttribute("goodsList", list);
 		return "main";
 	}
