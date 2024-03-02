30fc7cf00bb88a65f45e72b917b0441d0af11cb6
SHIN SUJUNG <shinssjj79@gmail.com>
2024-03-02 13:06:50 +0900

    fix: 주문 완료 페이지 Lombok 오류 수정 및 getter, setter 직접 추가

--- src/main/java/com/shoppingmall/dto/OrderDTO.java
+++ src/main/java/com/shoppingmall/dto/OrderDTO.java
@@ -5,8 +5,7 @@ import lombok.Setter;
 import org.apache.ibatis.type.Alias;
 
 @Alias("OrderDTO")
-@Getter
-@Setter
+
 public class OrderDTO {
 	int num;
 	String userid;
@@ -67,5 +66,133 @@ public class OrderDTO {
 				", payMethod='" + payMethod + '\'' +
 				", orderday='" + orderday + '\'' +
 				'}';
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public String getgCode() {
+		return gCode;
+	}
+
+	public String getgName() {
+		return gName;
+	}
+
+	public int getgPrice() {
+		return gPrice;
+	}
+
+	public String getgSize() {
+		return gSize;
+	}
+
+	public String getgColor() {
+		return gColor;
+	}
+
+	public int getgAmount() {
+		return gAmount;
+	}
+
+	public String getgImage() {
+		return gImage;
+	}
+
+	public String getOrderName() {
+		return orderName;
+	}
+
+	public String getZip_Code() {
+		return zip_Code;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getAddr_Detail() {
+		return addr_Detail;
+	}
+
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+
+	public String getPayMethod() {
+		return payMethod;
+	}
+
+	public String getOrderday() {
+		return orderday;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public void setgCode(String gCode) {
+		this.gCode = gCode;
+	}
+
+	public void setgName(String gName) {
+		this.gName = gName;
+	}
+
+	public void setgPrice(int gPrice) {
+		this.gPrice = gPrice;
+	}
+
+	public void setgSize(String gSize) {
+		this.gSize = gSize;
+	}
+
+	public void setgColor(String gColor) {
+		this.gColor = gColor;
+	}
+
+	public void setgAmount(int gAmount) {
+		this.gAmount = gAmount;
+	}
+
+	public void setgImage(String gImage) {
+		this.gImage = gImage;
+	}
+
+	public void setOrderName(String orderName) {
+		this.orderName = orderName;
+	}
+
+	public void setZip_Code(String zip_Code) {
+		this.zip_Code = zip_Code;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void setAddr_Detail(String addr_Detail) {
+		this.addr_Detail = addr_Detail;
+	}
+
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+
+	public void setPayMethod(String payMethod) {
+		this.payMethod = payMethod;
+	}
+
+	public void setOrderday(String orderday) {
+		this.orderday = orderday;
 	}
 }
