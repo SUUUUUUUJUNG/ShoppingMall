aea86c086053752a6c14c4f98ac4aec305f744d1
SHIN SUJUNG <shinssjj79@gmail.com>
2024-03-15 17:16:21 +0900

    Refactor OrderDTO for enhanced order details and timing
    - Rename id field to order_Id for clarity.
    - Add totalPrice field to include total cost calculation.
    - Change orderDay field type from String to LocalDateTime to accurately reflect order timings, defaulting to the current time.
    - Adjust fields ordering and inclusion for better data representation and consistency.
    
    This update is aimed at improving the management and tracking of order details within the system.

--- src/main/java/com/shoppingmall/dto/OrderDTO.java
+++ src/main/java/com/shoppingmall/dto/OrderDTO.java
@@ -4,12 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.apache.ibatis.type.Alias;
+
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @ToString
 @Alias("OrderDTO")
 public class OrderDTO {
-	Long id;
+	Long order_Id;
 	String userId;
 	String gCode;
 	String gName;
@@ -23,28 +26,6 @@ public class OrderDTO {
 	String addr_Detail;
 	String phoneNumber;
 	String payMethod;
-	String orderDay;
-
-	public OrderDTO() {
-	}
-
-	public OrderDTO(Long id, String userId, String gCode, String gName, int gPrice, String gSize, String gColor, int gAmount, String orderName, String zip_Code, String address, String addr_Detail, String phoneNumber, String payMethod, String orderDay) {
-		this.id = id;
-		this.userId = userId;
-		this.gCode = gCode;
-		this.gName = gName;
-		this.gPrice = gPrice;
-		this.gSize = gSize;
-		this.gColor = gColor;
-		this.gAmount = gAmount;
-		this.orderName = orderName;
-		this.zip_Code = zip_Code;
-		this.address = address;
-		this.addr_Detail = addr_Detail;
-		this.phoneNumber = phoneNumber;
-		this.payMethod = payMethod;
-		this.orderDay = orderDay;
-	}
-
-
+	Double totalPrice;
+	LocalDateTime orderDay = LocalDateTime.now();
 }
