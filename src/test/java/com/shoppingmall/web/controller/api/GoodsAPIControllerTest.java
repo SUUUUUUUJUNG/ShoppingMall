a6115e220575e97280f62d9dc085fa748cde5be4
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-11 15:52:57 +0900

    Refactor GoodsAPIControllerTest to include user role checks and extend testing functionality
    Refactored the GoodsAPIControllerTest to enhance the testing structure by including role-based security checks using @WithMockUser annotation. Added tests for new functionalities like updating goods information which involves a new GoodsUpdateRequestDTO. The test setup now ensures clearer role distinctions and tests interactions more comprehensively.

--- src/test/java/com/shoppingmall/web/controller/api/GoodsAPIControllerTest.java
+++ src/test/java/com/shoppingmall/web/controller/api/GoodsAPIControllerTest.java
@@ -4,6 +4,8 @@ package com.shoppingmall.web.controller.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shoppingmall.domain.dto.goods.GoodsCreateRequestDTO;
 import com.shoppingmall.domain.dto.goods.GoodsDTO;
+import com.shoppingmall.domain.dto.goods.GoodsResponseDTO;
+import com.shoppingmall.domain.dto.goods.GoodsUpdateRequestDTO;
 import com.shoppingmall.domain.dto.member.MemberDTO;
 import com.shoppingmall.domain.service.GoodsService;
 import com.shoppingmall.domain.service.MemberLoginService;
@@ -14,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,10 +25,8 @@ import java.security.Principal;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -44,7 +45,7 @@ class GoodsAPIControllerTest {
     @MockBean
     private MemberLoginService memberLoginService;
 
-    private GoodsCreateRequestDTO requestDTO;
+    private GoodsCreateRequestDTO createRequestDTO;
 
     private MemberDTO memberDTO;
 
@@ -53,10 +54,10 @@ class GoodsAPIControllerTest {
 
     @BeforeEach
     void setUp() {
-        requestDTO = new GoodsCreateRequestDTO();
-        requestDTO.setGName("New Product");
-        requestDTO.setGPrice(999);
-        requestDTO.setGContent("A description of the new product");
+        createRequestDTO = new GoodsCreateRequestDTO();
+        createRequestDTO.setGName("New Product");
+        createRequestDTO.setGPrice(999);
+        createRequestDTO.setGContent("A description of the new product");
 
         memberDTO = new MemberDTO();
         memberDTO.setMemberId(1L);
@@ -72,18 +73,19 @@ class GoodsAPIControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "testadmin",roles = "ADMIN")
     void createAsAdminShouldSucceed() throws Exception {
         when(memberLoginService.findByPrinciple(any())).thenReturn(memberDTO);
 
         mockMvc.perform(post("/api/goods")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(requestDTO))
-                .with(user("admin").roles("ADMIN")))
+                .content(objectMapper.writeValueAsString(createRequestDTO)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.message").value("상품이 정상적으로 등록되었습니다."));
     }
 
     @Test
+    @WithMockUser(username = "testuser",roles = "USER")
     void createAsUserShouldFail() throws Exception {
         memberDTO.setRole("ROLE_USER");
 
@@ -91,44 +93,56 @@ class GoodsAPIControllerTest {
 
         mockMvc.perform(post("/api/goods")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(requestDTO))
-                .with(user("user").roles("USER")))
+                .content(objectMapper.writeValueAsString(createRequestDTO)))
                 .andExpect(status().isUnauthorized());
     }
 
     @Test
+    @WithMockUser(username = "test")
     void findByCodeShouldReturnGoodsInfo() throws Exception {
         when(goodsService.findByCode(eq("001"))).thenReturn(sampleGoodsDTO);
-
+        GoodsResponseDTO responseDTO = new GoodsResponseDTO(sampleGoodsDTO);
+        String expectedJson = objectMapper.writeValueAsString(responseDTO);
         mockMvc.perform(get("/api/goods")
                         .param("gCode", "001")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.code").value("001"))
-                .andExpect(jsonPath("$.name").value("니트"))
-                .andExpect(jsonPath("$.price").value(9999))
-                .andExpect(jsonPath("$.image").value("top001.jpg"));
+                .andExpect(content().json(expectedJson, true));
     }
 
     @Test
+    @WithMockUser(username = "testAdmin",roles = "ADMIN")
     void deleteGoodsAsAdminShouldSucceed() throws Exception{
         String gCode = "001";
         when(memberLoginService.findByPrinciple(any())).thenReturn(memberDTO);
 
-        mockMvc.perform(delete("/api/goods/{gCode}",gCode)
-                .with(user("admin").roles("ADMIN")))
+        mockMvc.perform(delete("/api/goods/{gCode}",gCode))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.message").value("상품이 삭제되었습니다."));
     }
 
     @Test
+    @WithMockUser(username = "testUser",roles = "USER")
     void deleteGoodsAsUserShouldFail() throws Exception{
         String gCode = "001";
         memberDTO.setRole("ROLE_USER");
         when(memberLoginService.findByPrinciple(any())).thenReturn(memberDTO);
 
-        mockMvc.perform(delete("/api/goods/{gCode}",gCode)
-                .with(user("user").roles("USER")))
+        mockMvc.perform(delete("/api/goods/{gCode}",gCode))
                 .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "testAdmin",roles = "ADMIN")
+    void updateGoodsAsAdminShouldSucceed() throws Exception {
+        when(memberLoginService.findByPrinciple(any(Principal.class))).thenReturn(memberDTO);
+        when(goodsService.update(any())).thenReturn(1);  // Assume the update is successful
+        GoodsUpdateRequestDTO updateRequestDTO = new GoodsUpdateRequestDTO();
+
+        mockMvc.perform(patch("/api/goods")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updateRequestDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("상품이 수정되었습니다."));
     }
 }
