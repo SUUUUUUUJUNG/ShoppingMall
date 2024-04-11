4df2249cf0156c0a0bdbcd1b4e3c987fc29f2fcd
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-11 14:32:38 +0900

    Add GoodsUpdateRequestDTO for handling goods updates
    This commit introduces the GoodsUpdateRequestDTO class in the package com.shoppingmall.domain.dto.goods. The class includes fields such as gCode, gCategory, gName, gContent, gPrice, and gImage, along with Lombok annotations @Getter, @Setter, and @ToString to simplify the boilerplate code.

---
+++ src/main/java/com/shoppingmall/domain/dto/goods/GoodsUpdateRequestDTO.java
@@ -0,0 +1,19 @@
+package com.shoppingmall.domain.dto.goods;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class GoodsUpdateRequestDTO {
+
+    private String gCode;
+    private String gCategory;
+    private String gName;
+    private String gContent;
+    private int gPrice;
+    private String gImage;
+
+}
