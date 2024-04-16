50c83a87ce2c9da9107517e644c2198dd142bc94
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-16 23:20:34 +0900

    MemberLoginService에 비로그인 시 유효성 검사 로직 추가

--- src/main/java/com/shoppingmall/domain/service/MemberLoginService.java
+++ src/main/java/com/shoppingmall/domain/service/MemberLoginService.java
@@ -15,6 +15,9 @@ public class MemberLoginService {
     private final MemberService memberService;
 
     public MemberDTO findByPrinciple(Principal principal) {
+        if (principal == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "로그인이 필요한 서비스입니다.");
+        }
         MemberDTO memberDTO = memberService.findByUsername(principal.getName());
         if (memberDTO == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "회원이 존재하지 않습니다.");
