b4d456d4f1470778cd42f31c7980dc86012f32f8
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-17 15:11:36 +0900

    Add find all orders by member ID endpoint to OrdersAPIController
    - Implement a new GET endpoint in OrdersAPIController to retrieve all orders for the logged-in member.
    - Utilize the MemberLoginService to obtain the current member's details and the OrdersService to fetch the orders.

--- src/main/java/com/shoppingmall/web/controller/api/OrdersAPIController.java
+++ src/main/java/com/shoppingmall/web/controller/api/OrdersAPIController.java
@@ -1,14 +1,12 @@
 package com.shoppingmall.web.controller.api;
 
+import com.shoppingmall.domain.dto.member.MemberDTO;
 import com.shoppingmall.domain.dto.order.OrdersCreateRequestDTO;
 import com.shoppingmall.domain.service.MemberLoginService;
 import com.shoppingmall.domain.service.OrdersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.Map;
@@ -27,5 +25,11 @@ public class OrdersAPIController {
         ordersCreateRequestDTO.setMemberId(memberId);
         ordersService.create(ordersCreateRequestDTO);
         return ResponseEntity.ok(Map.of("message","상품이 추가되었습니다."));
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAllByMemberId(Principal principal){
+        MemberDTO memberDTO = memberLoginService.findByPrinciple(principal);
+        return ResponseEntity.ok(ordersService.findAllByMemberId(memberDTO.getMemberId()));
     }
 }
