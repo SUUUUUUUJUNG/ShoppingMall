ca6ac58a9ea3b296b01cd4ffd8efc70c7d9657ec
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-17 20:07:59 +0900

    Refactor OrdersAPIController and related tests for consistency
    - Renamed OrdersService to OrderService and OrdersDTO to OrderDTO to maintain naming consistency across the application.
    - Updated test cases in OrdersAPIControllerTest to reflect changes in service and DTO names.
    - Enhanced readability and maintainability of test cases by ensuring that service method names and usage are consistent with newly introduced naming conventions.

--- src/test/java/com/shoppingmall/web/controller/api/OrdersAPIControllerTest.java
+++ src/test/java/com/shoppingmall/web/controller/api/OrdersAPIControllerTest.java
@@ -2,11 +2,11 @@ package com.shoppingmall.web.controller.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shoppingmall.domain.dto.member.MemberDTO;
-import com.shoppingmall.domain.dto.order.OrdersCreateRequestDTO;
-import com.shoppingmall.domain.dto.order.OrdersDTO;
+import com.shoppingmall.domain.dto.order.OrderCreateRequestDTO;
+import com.shoppingmall.domain.dto.order.OrderDTO;
 import com.shoppingmall.domain.dto.payment.PaymentsCreateRequestDTO;
 import com.shoppingmall.domain.service.MemberLoginService;
-import com.shoppingmall.domain.service.OrdersService;
+import com.shoppingmall.domain.service.OrderService;
 import com.shoppingmall.domain.service.PaymentsService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -40,7 +40,7 @@ class OrdersAPIControllerTest {
     private ObjectMapper objectMapper;
 
     @MockBean
-    private OrdersService ordersService;
+    private OrderService orderService;
 
     @MockBean
     private PaymentsService paymentsService;
@@ -61,12 +61,12 @@ class OrdersAPIControllerTest {
     void createOrderShouldSucceed() throws Exception {
 
         when(memberLoginService.findByPrinciple(any(Principal.class))).thenReturn(memberDTO); // 가정: 회원 ID가 1로 설정된다.
-        when(ordersService.create(any(OrdersCreateRequestDTO.class))).thenReturn(1L); // 성공한 경우의 반환값
+        when(orderService.create(any(OrderCreateRequestDTO.class))).thenReturn(1L); // 성공한 경우의 반환값
         when(paymentsService.create(any(PaymentsCreateRequestDTO.class))).thenReturn(1); // 성공한 경우의 반환값
 
         mockMvc.perform(post("/api/orders")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(new OrdersCreateRequestDTO())))
+                        .content(objectMapper.writeValueAsString(new OrderCreateRequestDTO())))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.message").value("상품이 추가되었습니다."));
     }
@@ -75,14 +75,14 @@ class OrdersAPIControllerTest {
     @WithMockUser(username = "user1", roles = "USER")
     void findAllByMemberIdShouldReturnOrders() throws Exception {
         when(memberLoginService.findByPrinciple(any(Principal.class))).thenReturn(memberDTO);// 가정: 회원 ID가 1로 설정된다.
-        ArrayList<OrdersDTO> ordersDTOS = new ArrayList<>();
-        OrdersDTO ordersDTO = new OrdersDTO();
-        ordersDTO.setOrderId(1L);
-        ordersDTOS.add(ordersDTO);
-        when(ordersService.findAllByMemberId(memberDTO.getMemberId())).thenReturn(ordersDTOS); // 가정: 회원 ID가 1로 설정된다.
+        ArrayList<OrderDTO> orderDTOS = new ArrayList<>();
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setOrderId(1L);
+        orderDTOS.add(orderDTO);
+        when(orderService.findAllByMemberId(memberDTO.getMemberId())).thenReturn(orderDTOS); // 가정: 회원 ID가 1로 설정된다.
         mockMvc.perform(get("/api/orders")
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].orderId").value(ordersDTOS.get(0).getOrderId()));
+                .andExpect(jsonPath("$[0].orderId").value(orderDTOS.get(0).getOrderId()));
     }
 }
