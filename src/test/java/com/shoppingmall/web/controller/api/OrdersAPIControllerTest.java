a76a6a485a1b4f9196ecf32418b874bb580aaddd
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-17 15:33:31 +0900

    Add unit test for findAllByMemberId method in OrdersAPIControllerTest
    - Refactor setUp method to initialize MemberDTO instance before each test.
    - Implement a new test case 'findAllByMemberIdShouldReturnOrders' to verify that all orders for a given member ID are correctly fetched and returned.
    - Use Mockito to mock service layer methods and ensure the controller handles the data as expected.

--- src/test/java/com/shoppingmall/web/controller/api/OrdersAPIControllerTest.java
+++ src/test/java/com/shoppingmall/web/controller/api/OrdersAPIControllerTest.java
@@ -3,6 +3,7 @@ package com.shoppingmall.web.controller.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shoppingmall.domain.dto.member.MemberDTO;
 import com.shoppingmall.domain.dto.order.OrdersCreateRequestDTO;
+import com.shoppingmall.domain.dto.order.OrdersDTO;
 import com.shoppingmall.domain.service.MemberLoginService;
 import com.shoppingmall.domain.service.OrdersService;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,11 +18,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.security.Principal;
+import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -42,17 +43,18 @@ class OrdersAPIControllerTest {
     @MockBean
     private MemberLoginService memberLoginService;
 
-    private OrdersCreateRequestDTO createRequestDTO;
+    private MemberDTO memberDTO;
 
     @BeforeEach
     void setUp() {
+        memberDTO = new MemberDTO();
+        memberDTO.setMemberId(1L);
     }
 
     @Test
     @WithMockUser(username = "adminUser", roles = "ADMIN")
     void createOrderShouldSucceed() throws Exception {
-        MemberDTO memberDTO = new MemberDTO();
-        memberDTO.setMemberId(1L);
+
         when(memberLoginService.findByPrinciple(any(Principal.class))).thenReturn(memberDTO); // 가정: 회원 ID가 1로 설정된다.
         when(ordersService.create(any(OrdersCreateRequestDTO.class))).thenReturn(1); // 성공한 경우의 반환값
 
@@ -63,4 +65,18 @@ class OrdersAPIControllerTest {
                 .andExpect(jsonPath("$.message").value("상품이 추가되었습니다."));
     }
 
+    @Test
+    @WithMockUser(username = "user1", roles = "USER")
+    void findAllByMemberIdShouldReturnOrders() throws Exception {
+        when(memberLoginService.findByPrinciple(any(Principal.class))).thenReturn(memberDTO);// 가정: 회원 ID가 1로 설정된다.
+        ArrayList<OrdersDTO> ordersDTOS = new ArrayList<>();
+        OrdersDTO ordersDTO = new OrdersDTO();
+        ordersDTO.setOrderId(1L);
+        ordersDTOS.add(ordersDTO);
+        when(ordersService.findAllByMemberId(memberDTO.getMemberId())).thenReturn(ordersDTOS); // 가정: 회원 ID가 1로 설정된다.
+        mockMvc.perform(get("/api/orders")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].orderId").value(ordersDTOS.get(0).getOrderId()));
+    }
 }
