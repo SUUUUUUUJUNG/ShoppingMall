c4e1e7bbb9293b2d9478ac7c6b8aa9593e526f42
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-20 22:41:18 +0900

    Update Security Configuration for Enhanced Authentication
    - Enhanced SecurityConfig to integrate JWT authentication and OAuth2 login support.
    - Replaced traditional form login with stateless JWT authentication using custom filters such as JWTFilter and LoginFilter.
    - Configured OAuth2 login with custom redirection and user service through CustomOAuth2UserService and handled successful login with CustomAuthenticationSuccessHandler.
    - Disabled CSRF and HTTP Basic auth to align with stateless authentication.
    - Configured session management to be stateless to ensure that no session state is preserved between requests.

--- src/main/java/com/shoppingmall/security/SecurityConfig.java
+++ src/main/java/com/shoppingmall/security/SecurityConfig.java
@@ -1,23 +1,35 @@
 package com.shoppingmall.security;
 
+import com.shoppingmall.security.handler.CustomAuthenticationSuccessHandler;
+import com.shoppingmall.security.jwt.JWTFilter;
+import com.shoppingmall.security.jwt.JWTUtil;
+import com.shoppingmall.security.jwt.LoginFilter;
+import com.shoppingmall.security.security.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final CustomUserDetailsService customUserDetailsService;
-    private final LoginAuthenticationFailureHandler loginAuthenticationFailureHandler;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    @Value("${jwt.expiredMs}")
+    String expiredMs;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -44,27 +56,43 @@ public class SecurityConfig {
                                 .anyRequest().permitAll()
                 );
 
-        http.userDetailsService(customUserDetailsService);
-
         http
-                .formLogin((authorize) -> authorize
-                        .loginPage("/login")
-                        .loginProcessingUrl("/login-proc")
-                        .defaultSuccessUrl("/", true)
-                        .failureHandler(loginAuthenticationFailureHandler)
-                        .permitAll());
-
+                .oauth2Login(oauth2 -> oauth2
+                        .redirectionEndpoint(endpoint -> endpoint.baseUri("/oauth2/callback/*"))
+                        .userInfoEndpoint(endpoint -> endpoint.userService(customOAuth2UserService))
+                        .loginPage("/login/social")
+                        .successHandler(customAuthenticationSuccessHandler)
+                );
         http
                 .logout((logoutConfig) ->
                         logoutConfig
                                 .logoutUrl("/logout")
                                 .logoutSuccessUrl("/")
                                 .invalidateHttpSession(true)
+                                .deleteCookies("AuthToken")
                                 .permitAll());
+        //csrf disable
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+
+        //Form 로그인 방식 disable
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        //http basic 인증 방식 disable
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
 
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable);
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
+
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, Long.parseLong(expiredMs)), UsernamePasswordAuthenticationFilter.class);
+
+        //세션 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
