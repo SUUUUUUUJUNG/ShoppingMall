88342c417803d222a1a4a3899333efb0982882c6
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-21 16:59:09 +0900

    Add create method in DeliveryAddressesService for new address entries
    - Extended DeliveryAddressesService to include a new method `create` that leverages the create operation in DeliveryAddressesDAO.
    - The `create` method in the service layer now supports inserting new delivery address data using DeliveryAddressesCreateRequestDTO.

--- src/main/java/com/shoppingmall/domain/service/member/DeliveryAddressesService.java
+++ src/main/java/com/shoppingmall/domain/service/member/DeliveryAddressesService.java
@@ -1,6 +1,7 @@
 package com.shoppingmall.domain.service.member;
 
 import com.shoppingmall.domain.dao.member.DeliveryAddressesDAO;
+import com.shoppingmall.domain.dto.delivery.DeliveryAddressesCreateRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +11,7 @@ public class DeliveryAddressesService {
 
     private final DeliveryAddressesDAO deliveryAddressesDAO;
 
+    public Integer create(DeliveryAddressesCreateRequestDTO requestDTO) {
+        return deliveryAddressesDAO.create(requestDTO);
+    }
 }
