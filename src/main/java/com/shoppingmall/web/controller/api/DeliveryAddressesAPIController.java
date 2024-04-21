e005415b3eb8f9a9b7437377f983e4c389edd8ba
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-21 17:01:56 +0900

    Enhance DeliveryAddressesAPIController with new create endpoint
    - Added a new POST endpoint in DeliveryAddressesAPIController for creating delivery addresses.
    - Integrated DeliveryAddressesCreateRequestDTO for handling incoming data.
    - Implemented logic to extract memberId from Principal and pass it to the service layer.
    - Used ResponseEntity to return a success message upon successful address creation.

--- src/main/java/com/shoppingmall/web/controller/api/DeliveryAddressesAPIController.java
+++ src/main/java/com/shoppingmall/web/controller/api/DeliveryAddressesAPIController.java
@@ -1,10 +1,17 @@
 package com.shoppingmall.web.controller.api;
 
+import com.shoppingmall.domain.dto.delivery.DeliveryAddressesCreateRequestDTO;
 import com.shoppingmall.domain.service.member.DeliveryAddressesService;
 import com.shoppingmall.domain.service.member.MemberLoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,4 +20,11 @@ public class DeliveryAddressesAPIController {
 
     private final MemberLoginService memberLoginService;
     private DeliveryAddressesService deliveryAddressesService;
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody DeliveryAddressesCreateRequestDTO requestDTO, Principal principal) {
+        Long memberId = memberLoginService.findByPrinciple(principal).getMemberId();
+        deliveryAddressesService.create(requestDTO);
+        return ResponseEntity.ok(Map.of("message","주소가 추가되었습니다."));
+    }
 }
