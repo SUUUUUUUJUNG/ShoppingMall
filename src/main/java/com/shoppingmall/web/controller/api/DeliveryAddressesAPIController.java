27fac9542784966970f32c1a681f03999ab9a77b
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-22 16:20:43 +0900

    Add Delete Endpoint to DeliveryAddressesAPIController
    Added a new DELETE endpoint in the `DeliveryAddressesAPIController` to allow for the deletion of delivery addresses. Implemented a `validateIsInquiryOwner` method to ensure that the principal user has the authority to delete the specified delivery address, enhancing security and user-specific access control.

--- src/main/java/com/shoppingmall/web/controller/api/DeliveryAddressesAPIController.java
+++ src/main/java/com/shoppingmall/web/controller/api/DeliveryAddressesAPIController.java
@@ -1,12 +1,17 @@
 package com.shoppingmall.web.controller.api;
 
 import com.shoppingmall.domain.dto.delivery.DeliveryAddressesCreateRequestDTO;
+import com.shoppingmall.domain.dto.delivery.DeliveryAddressesDTO;
+import com.shoppingmall.domain.dto.delivery.DeliveryAddressesUpdateRequestDTO;
 import com.shoppingmall.domain.dto.member.MemberDTO;
+import com.shoppingmall.domain.dto.review.ReviewDTO;
 import com.shoppingmall.domain.service.member.DeliveryAddressesService;
 import com.shoppingmall.domain.service.member.MemberLoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 import java.util.Map;
@@ -31,4 +36,28 @@ public class DeliveryAddressesAPIController {
         MemberDTO member = memberLoginService.findByPrinciple(principal);
         return ResponseEntity.ok(deliveryAddressesService.findAllByMemberId(member.getMemberId()));
     }
+
+   @DeleteMapping("/{Id}")
+   public ResponseEntity<?> delete(@PathVariable("Id") Long Id, Principal principal) {
+        validateIsInquiryOwner(Id, principal);
+        deliveryAddressesService.delete(Id);
+        return ResponseEntity.ok(Map.of("message","배송지가 삭제되었습니다."));
+   }
+
+    private void validateIsInquiryOwner(Long Id, Principal principal) {
+        Long memberId = memberLoginService.findByPrinciple(principal).getMemberId();
+        DeliveryAddressesDTO deliveryAddressesDTO = deliveryAddressesService.findById(Id);
+
+        if(deliveryAddressesDTO==null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"배송지를 찾을 수 없습니다.");
+        }
+        if(!deliveryAddressesDTO.getMemberId().equals(memberId)){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"권한이 없습니다.");
+        }
+
+
+    }
+
+
+
 }
