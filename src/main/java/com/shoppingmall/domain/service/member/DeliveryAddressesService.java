0ceb3dbee19db4d8ec274e258fd624af00784500
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-22 12:58:51 +0900

    Add findAllByMemberId method to DeliveryAddressesService
    - Extended the DeliveryAddressesService class to include the `findAllByMemberId` method.
    - This method utilizes the DAO layer to fetch all delivery addresses associated with a given memberId.

--- src/main/java/com/shoppingmall/domain/service/member/DeliveryAddressesService.java
+++ src/main/java/com/shoppingmall/domain/service/member/DeliveryAddressesService.java
@@ -2,8 +2,11 @@ package com.shoppingmall.domain.service.member;
 
 import com.shoppingmall.domain.dao.member.DeliveryAddressesDAO;
 import com.shoppingmall.domain.dto.delivery.DeliveryAddressesCreateRequestDTO;
+import com.shoppingmall.domain.dto.delivery.DeliveryAddressesDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +16,9 @@ public class DeliveryAddressesService {
 
     public Integer create(DeliveryAddressesCreateRequestDTO requestDTO) {
         return deliveryAddressesDAO.create(requestDTO);
+    }
+
+    public List<DeliveryAddressesDTO> findAllByMemberId(Long memberId) {
+        return deliveryAddressesDAO.findAllByMemberId(memberId);
     }
 }
