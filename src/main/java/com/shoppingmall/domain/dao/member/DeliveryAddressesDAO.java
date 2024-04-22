83189c04c28ef8ae52509f307d47d46db7e6a179
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-22 16:18:41 +0900

    Implement findById and delete Methods in DeliveryAddressesDAO
    Implemented 'findById' and 'delete' methods in the `DeliveryAddressesDAO` class and updated the corresponding XML mapper to support these operations. This allows for individual retrieval and deletion of delivery addresses by ID, enhancing the management capabilities of the delivery addresses.

--- src/main/java/com/shoppingmall/domain/dao/member/DeliveryAddressesDAO.java
+++ src/main/java/com/shoppingmall/domain/dao/member/DeliveryAddressesDAO.java
@@ -2,6 +2,7 @@ package com.shoppingmall.domain.dao.member;
 
 import com.shoppingmall.domain.dto.delivery.DeliveryAddressesCreateRequestDTO;
 import com.shoppingmall.domain.dto.delivery.DeliveryAddressesDTO;
+import com.shoppingmall.domain.dto.delivery.DeliveryAddressesUpdateRequestDTO;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -20,5 +21,14 @@ public class DeliveryAddressesDAO {
 
     public List<DeliveryAddressesDTO> findAllByMemberId(Long memberId) {
         return session.selectList("DeliveryAddressesMapper.findAllByMemberId", memberId);
+    }
+
+    public DeliveryAddressesDTO findById(Long id) {
+        return session.selectOne("DeliveryAddressesMapper.findById", id);
+    }
+
+
+    public int delete(Long id) {
+        return session.delete("DeliveryAddressesMapper.delete", id);
     }
 }
