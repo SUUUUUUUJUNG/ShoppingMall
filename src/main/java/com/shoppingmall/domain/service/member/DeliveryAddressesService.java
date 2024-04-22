627e7c760887719a126c8889f00a0358ff9113d2
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-22 16:20:13 +0900

    Extend DeliveryAddressesService with findById and delete Methods
    Enhanced the `DeliveryAddressesService` class by adding `findById` and `delete` methods to support retrieval and deletion of delivery addresses by ID. These methods utilize the existing functionality of the `DeliveryAddressesDAO` to offer more comprehensive management capabilities for delivery addresses.

--- src/main/java/com/shoppingmall/domain/service/member/DeliveryAddressesService.java
+++ src/main/java/com/shoppingmall/domain/service/member/DeliveryAddressesService.java
@@ -3,6 +3,7 @@ package com.shoppingmall.domain.service.member;
 import com.shoppingmall.domain.dao.member.DeliveryAddressesDAO;
 import com.shoppingmall.domain.dto.delivery.DeliveryAddressesCreateRequestDTO;
 import com.shoppingmall.domain.dto.delivery.DeliveryAddressesDTO;
+import com.shoppingmall.domain.dto.delivery.DeliveryAddressesUpdateRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +21,14 @@ public class DeliveryAddressesService {
 
     public List<DeliveryAddressesDTO> findAllByMemberId(Long memberId) {
         return deliveryAddressesDAO.findAllByMemberId(memberId);
+    }
+
+    public DeliveryAddressesDTO findById(Long id) {
+        return deliveryAddressesDAO.findById(id);
+    }
+
+
+    public int delete(Long id) {
+        return deliveryAddressesDAO.delete(id);
     }
 }
