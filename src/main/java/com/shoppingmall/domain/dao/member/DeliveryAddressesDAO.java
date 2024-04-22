32a0a7cf1dd953975dc5cdb583f51f082b3cf846
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-22 12:57:56 +0900

    Add findAllByMemberId method to DeliveryAddressesMapper and DAO
    - Updated the DeliveryAddressesMapper XML to include a new select query `findAllByMemberId`, which retrieves all delivery addresses for a specific memberId.
    - Modified the DeliveryAddressesDAO to include the `findAllByMemberId` method that uses the new select query to fetch delivery addresses from the database.

--- src/main/java/com/shoppingmall/domain/dao/member/DeliveryAddressesDAO.java
+++ src/main/java/com/shoppingmall/domain/dao/member/DeliveryAddressesDAO.java
@@ -1,9 +1,12 @@
 package com.shoppingmall.domain.dao.member;
 
 import com.shoppingmall.domain.dto.delivery.DeliveryAddressesCreateRequestDTO;
+import com.shoppingmall.domain.dto.delivery.DeliveryAddressesDTO;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class DeliveryAddressesDAO {
@@ -13,5 +16,9 @@ public class DeliveryAddressesDAO {
 
     public Integer create(DeliveryAddressesCreateRequestDTO requestDTO) {
         return session.insert("DeliveryAddressesMapper.create",requestDTO);
+    }
+
+    public List<DeliveryAddressesDTO> findAllByMemberId(Long memberId) {
+        return session.selectList("DeliveryAddressesMapper.findAllByMemberId", memberId);
     }
 }
