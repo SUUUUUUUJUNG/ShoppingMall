73e16877233d273d8a86a6a477638d9ac1096e73
SHIN SUJUNG <shinssjj79@gmail.com>
2024-04-22 21:19:41 +0900

    Enhanced DeliveryAddressesAPIController with new functionality
    - Added MemberService dependency to handle operations related to member's primary delivery address.
    - Introduced a new endpoint '/setPrimary/{id}' to set a delivery address as the primary one for a user.
    - Adjusted existing methods to incorporate changes from MemberService, ensuring that member details are updated when delivery addresses are manipulated.

--- src/main/java/com/shoppingmall/web/controller/api/DeliveryAddressesAPIController.java
+++ src/main/java/com/shoppingmall/web/controller/api/DeliveryAddressesAPIController.java
@@ -4,9 +4,9 @@ import com.shoppingmall.domain.dto.delivery.DeliveryAddressesCreateRequestDTO;
 import com.shoppingmall.domain.dto.delivery.DeliveryAddressesDTO;
 import com.shoppingmall.domain.dto.delivery.DeliveryAddressesUpdateRequestDTO;
 import com.shoppingmall.domain.dto.member.MemberDTO;
-import com.shoppingmall.domain.dto.review.ReviewDTO;
 import com.shoppingmall.domain.service.member.DeliveryAddressesService;
 import com.shoppingmall.domain.service.member.MemberLoginService;
+import com.shoppingmall.domain.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,12 +23,21 @@ public class DeliveryAddressesAPIController {
 
     private final MemberLoginService memberLoginService;
     private final DeliveryAddressesService deliveryAddressesService;
+    private final MemberService memberService;
 
     @PostMapping
     public ResponseEntity<?> create(@RequestBody DeliveryAddressesCreateRequestDTO requestDTO, Principal principal) {
-        Long memberId = memberLoginService.findByPrinciple(principal).getMemberId();
+        requestDTO.setMemberId(memberLoginService.findByPrinciple(principal).getMemberId());
         deliveryAddressesService.create(requestDTO);
-        return ResponseEntity.ok(Map.of("message","주소가 추가되었습니다."));
+        return ResponseEntity.ok(Map.of("message", "주소가 추가되었습니다."));
+    }
+
+    @PostMapping("/setPrimary/{id}")
+    public ResponseEntity<?> setPrimary(@PathVariable("id") Long id, Principal principal) {
+        Long memberId = memberLoginService.findByPrinciple(principal).getMemberId();
+        DeliveryAddressesDTO deliveryAddressesDTO = deliveryAddressesService.findById(id);
+        memberService.setPrimaryAddress(deliveryAddressesDTO);
+        return ResponseEntity.ok(Map.of("message", "대표배송지로 설정되었습니다."));
     }
 
     @GetMapping
@@ -37,29 +46,36 @@ public class DeliveryAddressesAPIController {
         return ResponseEntity.ok(deliveryAddressesService.findAllByMemberId(member.getMemberId()));
     }
 
-   @DeleteMapping("/{id}")
-   public ResponseEntity<?> delete(@PathVariable("id") Long Id, Principal principal) {
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable("id") Long id, Principal principal) {
+        validateIsDeliveryAddressesOwner(id, principal);
+        return ResponseEntity.ok(deliveryAddressesService.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long Id, Principal principal) {
         validateIsDeliveryAddressesOwner(Id, principal);
         deliveryAddressesService.delete(Id);
-        return ResponseEntity.ok(Map.of("message","배송지가 삭제되었습니다."));
-   }
+        return ResponseEntity.ok(Map.of("message", "배송지가 삭제되었습니다."));
+    }
 
-   @PatchMapping
-   public ResponseEntity<?> update(@RequestBody DeliveryAddressesUpdateRequestDTO requestDTO, Principal principal) {
-        validateIsDeliveryAddressesOwner(requestDTO.getId(),principal);
+    @PatchMapping
+    public ResponseEntity<?> update(@RequestBody DeliveryAddressesUpdateRequestDTO requestDTO, Principal principal) {
+        validateIsDeliveryAddressesOwner(requestDTO.getId(), principal);
+        requestDTO.setMemberId(memberLoginService.findByPrinciple(principal).getMemberId());
         deliveryAddressesService.update(requestDTO);
-        return ResponseEntity.ok(Map.of("message","배송지가 수정되었습니다."));
-   }
+        return ResponseEntity.ok(Map.of("message", "배송지가 수정되었습니다."));
+    }
 
     private void validateIsDeliveryAddressesOwner(Long Id, Principal principal) {
         Long memberId = memberLoginService.findByPrinciple(principal).getMemberId();
         DeliveryAddressesDTO deliveryAddressesDTO = deliveryAddressesService.findById(Id);
 
-        if(deliveryAddressesDTO==null){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"배송지를 찾을 수 없습니다.");
+        if (deliveryAddressesDTO == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "배송지를 찾을 수 없습니다.");
         }
-        if(!deliveryAddressesDTO.getMemberId().equals(memberId)){
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"권한이 없습니다.");
+        if (!deliveryAddressesDTO.getMemberId().equals(memberId)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "권한이 없습니다.");
         }
     }
 }
