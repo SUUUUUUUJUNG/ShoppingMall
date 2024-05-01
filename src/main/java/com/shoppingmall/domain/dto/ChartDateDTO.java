fad2ad65c4782b3ffa6d8285735b99a38f87ce2f
SHIN SUJUNG <shinssjj79@gmail.com>
2024-05-01 23:12:03 +0900

    Add ChartDateDTO for managing date and total sales

---
+++ src/main/java/com/shoppingmall/domain/dto/ChartDateDTO.java
@@ -0,0 +1,15 @@
+package com.shoppingmall.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@ToString
+public class ChartDateDTO {
+    private String date; // 날짜를 'YYYY-MM-DD' 형식의 문자열로 관리
+    private BigDecimal totalSales; // 해당 날짜의 총 판매액
+}
