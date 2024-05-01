ef12c995efde12ee1df183d5444c8ff62d452770
SHIN SUJUNG <shinssjj79@gmail.com>
2024-05-01 23:13:10 +0900

    Add queries for retrieving sales data for chart visualization

--- src/main/java/com/shoppingmall/domain/dao/order/OrderDAO.java
+++ src/main/java/com/shoppingmall/domain/dao/order/OrderDAO.java
@@ -1,5 +1,6 @@
 package com.shoppingmall.domain.dao.order;
 
+import com.shoppingmall.domain.dto.ChartDateDTO;
 import com.shoppingmall.domain.dto.order.OrderCreateRequestDTO;
 import com.shoppingmall.domain.dto.order.OrderDTO;
 import lombok.RequiredArgsConstructor;
@@ -15,14 +16,26 @@ public class OrderDAO {
     private final SqlSessionTemplate session;
 
     public void create(OrderCreateRequestDTO orderCreateRequestDTO) {
-       session.insert("OrderMapper.create", orderCreateRequestDTO);
+        session.insert("OrderMapper.create", orderCreateRequestDTO);
     }
 
     public List<OrderDTO> findAllByMemberId(Long memberId) {
-        return session.selectList("OrderMapper.findAllByMemberId",memberId);
+        return session.selectList("OrderMapper.findAllByMemberId", memberId);
     }
 
     public OrderDTO findById(Long orderId) {
         return session.selectOne("OrderMapper.findById", orderId);
+    }
+
+    public List<ChartDateDTO> selectSalesPast30Days() {
+        return session.selectList("OrderMapper.selectSalesPast30Days");
+    }
+
+    public List<ChartDateDTO> selectMonthlySalesPastYear() {
+        return session.selectList("OrderMapper.selectMonthlySalesPastYear");
+    }
+
+    public List<ChartDateDTO> selectYearlySalesPastThreeYears() {
+        return session.selectList("OrderMapper.selectYearlySalesPastThreeYears");
     }
 }
